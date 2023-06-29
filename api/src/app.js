61b2349a408ff939c0550b09d113fcd6a6c931aa
SebastianGaviriaM <70300843+SebastianGaviriaM@users.noreply.github.com>
2023-06-29 18:31:10 -0500

    asterisco

--- api/src/app.js
+++ api/src/app.js
@@ -13,17 +13,17 @@ server.name = 'API';
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
-
+server.use(cors());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://turnero-o1b6.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
-server.use(cors());
+
 
 server.use('/', routes);
 
