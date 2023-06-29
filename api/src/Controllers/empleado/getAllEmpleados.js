c325834d93ef6021f09848e215a73f564e5bef7b
SebastianGaviriaM <70300843+SebastianGaviriaM@users.noreply.github.com>
2023-06-29 18:18:28 -0500

    Creacion nuevo admin

--- api/src/Controllers/empleado/getAllEmpleados.js
+++ api/src/Controllers/empleado/getAllEmpleados.js
@@ -1,14 +1,34 @@
 const {Empleado} = require('../../db');
+const bcrypt = require('bcryptjs'); 
 
 const getAllEmpleados = async () => {
 
-    const empleados = await Empleado.findAll({
-        where: {isSede:false, isAdmin: false}
+    const admin = await Empleado.findAll({
+        where: {isAdmin: true}
     });
+    
+    if(!admin){
+        const passwordhash = bcrypt.hashSync("micelu2023", parseInt(10));
+        await Empleado.create({
+            nombre: "admin1",
+            sede:"Medellin",
+            password: passwordhash,
+            isAdmin:true
+        })
 
-    return empleados;
+        return await Empleado.findAll({
+            where: {isAdmin: true}
+        });;
 
+    }
+    else{
+        const empleados = await Empleado.findAll({
+            where: {isSede:false, isAdmin: false}
+        });
+        return empleados;
+    
 
+    }
 }
 
 module.exports = getAllEmpleados;
