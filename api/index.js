58eefb9a67ee796e6055d8af4c34164bca0a6803
SebastianGaviriaM <70300843+SebastianGaviriaM@users.noreply.github.com>
2023-06-29 19:01:56 -0500

    Correccion de puerto

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db.js");
 require('dotenv').config();
 
 conn.sync({ alter: true }).then(() => {
-  server.listen(process.env.DB_PORT, () => {
-    console.log("Server is listening at port > ", process.env.DB_PORT);
+  server.listen(process.env.PORT, () => {
+    console.log("Server is listening at port > ", process.env.PORT);
   });
 });
