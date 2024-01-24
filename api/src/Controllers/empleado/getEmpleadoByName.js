8a3b64d23c4e0ee86300a2bd7d067ba404813591
David Cardona Duque <davcardona@unal.edu.co>
2024-01-23 21:49:48 -0500

    Update getEmpleadoByName.js

--- api/src/Controllers/empleado/getEmpleadoByName.js
+++ api/src/Controllers/empleado/getEmpleadoByName.js
@@ -1,14 +1,11 @@
-const {Empleado} = require('../../db').conn2;
+const { conn2 } = require('../../db');
 
 const getEmpleadoByName = async (usuario) => {
-
-    const empleadoSeleccionado = await Empleado.findOne({
-        where: {nombre:usuario}
+    const empleadoSeleccionado = await conn2.models.Empleado.findOne({
+        where: { nombre: usuario }
     });
 
     return empleadoSeleccionado;
-
-
 }
 
 module.exports = getEmpleadoByName;
