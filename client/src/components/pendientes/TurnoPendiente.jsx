2b5250f869294dfadd0d83453517de66c77fc222
desarrollomicelu <auxdesarrollo@micelu.co>
2024-07-23 18:52:37 -0500

    numero_orden

--- client/src/components/pendientes/TurnoPendiente.jsx
+++ client/src/components/pendientes/TurnoPendiente.jsx
@@ -1,38 +1,39 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import style from './pendientes.module.css';
-import {useNavigate} from 'react-router-dom';
-import {putTurno, verificarToken} from '../../redux/actions';
+import { useNavigate } from 'react-router-dom';
+import { putTurno, verificarToken } from '../../redux/actions';
 import { useDispatch } from 'react-redux';
 
-const TurnoPendiente = ({id, nombre, razon, celular,comoNosConociste}) => {
-
+const TurnoPendiente = ({ id, nombre, razon, celular, comoNosConociste, orderNumber }) => {
   const navigate = useNavigate();
-  const dispatch = useDispatch(); 
-  
-  const handleAtender = async()=>{
-    
+  const dispatch = useDispatch();
+
+  const handleAtender = async () => {
     const hora = new Date();
     const tokenData = await dispatch(verificarToken(localStorage.getItem('token')));
     dispatch(putTurno(id, hora, tokenData.info.id));
-
     navigate("/atendiendo", {
       state: {
-        id:id, 
-        nombre, 
+        id: id,
+        nombre,
         razon,
         celular,
         comoNosConociste
-      }});
-
-    
+      }
+    });
   }
 
   return (
     <div className={`${style.clientep} border`}>
-          <p className={`${style.datos1}`}>{nombre}</p>
-          <p className={`${style.datos1}`}>{razon}</p>
-          <button className={`${style.boton2}`} onClick={handleAtender}>Atender <i className="bi bi-check"></i></button>
+      <p className={`${style.datos1}`}>{nombre}</p>
+      <p className={`${style.datos1}`}>{razon}</p>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <button className={`${style.boton2}`} onClick={handleAtender}>
+          Atender <i className="bi bi-check"></i>
+        </button>
+        <span className={`${style.orderNumber}`}>{orderNumber}</span>
+      </div>
     </div>
   );
 };
